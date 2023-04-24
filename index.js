eab4bd78e571e55f5407adb82c06b1a5451148e1
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2023-04-24 16:24:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Login(event) {
 
   if (a == "Harshini_Medha" && b == "root123@gmail.com" && c == "12345") {
     localStorage.setItem("user", "harshini");
-    window.location.href = "http://127.0.0.1:5500/dash.html";
+    window.location.href = "https://green-land-collections.vercel.app/dash.html";
   }
   else{
     alert("wrong Password or username")
@@ -17,7 +17,7 @@ function Verify() {
   let x = localStorage.getItem("user") === "harshini";
 
   if (x) {
-    window.location.href = "http://127.0.0.1:5500/dash.html";
+    window.location.href = "https://green-land-collections.vercel.app/dash.html";
   } else {
   }
 }
--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 function Logout() {
   localStorage.removeItem("user");
-  window.location.href = "http://127.0.0.1:5500/";
+  window.location.href = "/";
 }
 
 
