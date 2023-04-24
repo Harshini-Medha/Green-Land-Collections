9184bcd83a8536fc8da48cb6014068d7ab90cc43
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2023-04-24 16:31:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function Login(event) {
 
   if (a == "Harshini_Medha" && b == "root123@gmail.com" && c == "12345") {
     localStorage.setItem("user", "harshini");
-    window.location.href = "https://green-land-collections.vercel.app/dash.html";
-  }
-  else{
-    alert("wrong Password or username")
+    window.location.href =
+      "https://green-land-collections.vercel.app/dash.html";
+  } else {
+    alert("wrong Password or username");
   }
 }
 
@@ -17,7 +17,8 @@ function Verify() {
   let x = localStorage.getItem("user") === "harshini";
 
   if (x) {
-    window.location.href = "https://green-land-collections.vercel.app/dash.html";
+    window.location.href =
+      "https://green-land-collections.vercel.app/dash.html";
   } else {
   }
 }
@@ -26,8 +27,6 @@ Verify();
 
 function Verifypas() {
   let a = document.getElementById("pass");
-
-
 
   if (a.type === "password") {
     a.type = "text";
